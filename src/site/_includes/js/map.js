e746539d9516c00af477fb9d2f324709f9181f6e
3DPrintScanner <1988anthonythomas@gmail.com>
2020-03-18 09:51:32 +0000

    Add tooltip to map

--- src/site/_includes/js/map.js
+++ src/site/_includes/js/map.js
@@ -7,20 +7,40 @@ zoom: 12
 });
  
 map.on('load', function() {
-map.addSource('clerkenwell', {
-'type': 'geojson',
-'data': '/data/wards.geojson'
-});
-map.addLayer({
-'id': 'maine',
-'type': 'fill',
-'source': 'clerkenwell',
-'layout': {},
-'paint': {
-'fill-color': '#088',
-'fill-opacity': 0.5
-}
-});
-map.addControl(new mapboxgl.FullscreenControl());
+    map.addSource('clerkenwell', {
+    'type': 'geojson',
+    'data': '/data/wards.geojson'
+    });
+    map.addLayer({
+    'id': 'wards-layer',
+    'type': 'fill',
+    'source': 'clerkenwell',
+    'layout': {},
+    'paint': {
+    'fill-color': '#088',
+    'fill-opacity': 0.5
+    }
+    });
+
+
+    map.addControl(new mapboxgl.FullscreenControl());
+
+
+    map.on('click', 'wards-layer', function(e) {
+        new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML(`<p style="color:black;">${e.features[0].properties.NAME}</p>`)
+        .addTo(map);
+        });
+        
+        // Change the cursor to a pointer when the mouse is over the states layer.
+    map.on('mouseenter', 'wards-layer', function() {
+    map.getCanvas().style.cursor = 'pointer';
+    });
+    
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'wards-layer', function() {
+    map.getCanvas().style.cursor = '';
+    });
 });
 
